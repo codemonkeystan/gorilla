7e265aa6b331afe48c2a05a64099ed1c360eef80
Satchit Shenoy <satchit.shenoy@runtastic.com>
2021-07-17 19:30:49 +0200

    Structuring our app

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+// Contains points to database connections
+type App struct {
+}
+
+// Sets up our appli
+func (app *App) Run() error {
+	fmt.Println("Setting up our APP")
+	return nil
+}
+
 func main() {
 	fmt.Println("Go rest API course")
+
+	app := App{}
+
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up REST API", err)
+	}
 }
